7f76ad82f2f48f5ff5738238e96c8e737e74dbac
juanxo13597 <juanxo97master@gmail.com>
2023-01-06 19:02:04 +0100

    config proxy

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private HttpClient:HttpClient){
+    this.HttpClient.get('/api').subscribe({
+      next(value) {
+          console.log(value);
+      },
+      error(err) {
+          console.log(err);
+      },
+    })
+  }
   title = 'front';
 }
