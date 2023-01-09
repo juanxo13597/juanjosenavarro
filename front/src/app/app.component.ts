60e3399586abe07e0b73ed15dfbe51a70af66faf
juanxo13597 <juanxo97master@gmail.com>
2023-01-09 16:35:00 +0100

    fix test angular CI

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private HttpClient: HttpClient) {
-    this.HttpClient.get('/api').subscribe({
-      next(value) {
-        console.log(value);
-      },
-      error(err) {
-        console.log(err);
-      },
-    });
-  }
   title = 'front';
 }
